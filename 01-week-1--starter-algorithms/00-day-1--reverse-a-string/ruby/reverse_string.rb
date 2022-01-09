254024da94e19c1fb406b2353560d6d69ea0e268
FluxOfPingIntegers <Ryan.M.Schleck@gmail.com>
2022-01-09 18:15:40 -0500

    pseudo code for ruby reverse string

--- 01-week-1--starter-algorithms/00-day-1--reverse-a-string/ruby/reverse_string.rb
+++ 01-week-1--starter-algorithms/00-day-1--reverse-a-string/ruby/reverse_string.rb
@@ -15,4 +15,19 @@ if __FILE__ == $PROGRAM_NAME
 end
 
 # Please add your pseudocode to this file
+=begin
+  -take in a string, convert it to an array
+  -create a hash with key values of indexes of the array pointing to array elements
+  -create a new blank array
+  -iterate over the hash using a variable that you will decrement, this variable should start at the
+   highest index of the has, so its initial value should be the original arrays length - 1, push these
+   values into the new array as you iterate
+  -return the joined new array
+=end
 # And a written explanation of your solution
+=begin
+  I believe mapping the string to an array is the first step to being able to iterate through each word of the string.
+  Once this is done, having this array correspond to a hash will reduce the time of further iteration whilst I reverse
+  its order.  This new order will be saved in a new array.  Once this array is converted back to a string that string
+  should be returned.
+=end
