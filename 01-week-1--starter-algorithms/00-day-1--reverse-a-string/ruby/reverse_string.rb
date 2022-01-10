f62b685403dbb10d580341a60bf268fdfdb54c1a
FluxOfPingIntegers <Ryan.M.Schleck@gmail.com>
2022-01-10 10:16:45 -0500

    ruby solve for reversed string

--- 01-week-1--starter-algorithms/00-day-1--reverse-a-string/ruby/reverse_string.rb
+++ 01-week-1--starter-algorithms/00-day-1--reverse-a-string/ruby/reverse_string.rb
@@ -1,12 +1,19 @@
 def reverse_string(str)
   # type your code in here
+  str_array = str.split("")
+  reversed_str = ""
+  str_array.each do |char|
+    reversed_str = char + reversed_str
+  end
+  reversed_str
 end
+
 
 if __FILE__ == $PROGRAM_NAME
   puts "Expecting: 'ih'"
   puts "=>", reverse_string('hi')
 
-  puts
+  puts 
 
   puts "Expecting: 'ybabtac'"
   puts "=>", reverse_string('catbaby')
@@ -17,17 +24,16 @@ end
 # Please add your pseudocode to this file
 =begin
   -take in a string, convert it to an array
-  -create a hash with key values of indexes of the array pointing to array elements
-  -create a new blank array
-  -iterate over the hash using a variable that you will decrement, this variable should start at the
-   highest index of the has, so its initial value should be the original arrays length - 1, push these
-   values into the new array as you iterate
-  -return the joined new array
+  -declare a variable as an empty string
+  -iterate over the created array, adding each element to the start of the empty string variable and redefining that
+   variable
+  -return the variable
 =end
 # And a written explanation of your solution
 =begin
-  I believe mapping the string to an array is the first step to being able to iterate through each word of the string.
-  Once this is done, having this array correspond to a hash will reduce the time of further iteration whilst I reverse
-  its order.  This new order will be saved in a new array.  Once this array is converted back to a string that string
-  should be returned.
+  creating an array of the string seems like the easiest way to iterate over the characters of the string.
+  at first I thought this array would then have to be converted to a hash in order to take advantage of hash
+  lookup times, and then initiate another "reversed_array" and use a counter to iterate across the keys of the hash
+  using a while loop and assigning these keys from greatest to least to the reversed_array.  Then return the joined
+  reversed array.
 =end
