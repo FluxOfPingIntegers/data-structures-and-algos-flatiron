eed5baf9f52ef69c562cc5c261f677a8270f1a28
FluxOfPingIntegers <Ryan.M.Schleck@gmail.com>
2022-01-11 11:08:51 -0500

    javascript solve for find_first_duplicate

--- 01-week-1--starter-algorithms/01-day-2--find-first-duplicate/javascript/find_first_duplicate.js
+++ 01-week-1--starter-algorithms/01-day-2--find-first-duplicate/javascript/find_first_duplicate.js
@@ -1,5 +1,16 @@
 function findFirstDuplicate(arr) {
   // type your code here
+  const findMatchIndex = element => arr.indexOf(element)
+
+  const matchTest = mappedArray => mappedArray.filter(indexValue => indexValue !== -1)
+
+  const firstMatch = filteredArray => filteredArray.find((element, index) => element == filteredArray[index + 1])
+
+  const matchFind = filteredArray => filteredArray.length > 1 ? arr[firstMatch(filteredArray)] : -1
+
+  const arrayMatchFinder = arr => matchFind(matchTest(arr.map(element => findMatchIndex(element))))
+
+  return arrayMatchFinder(arr)    
 }
 
 if (require.main === module) {
@@ -16,4 +27,17 @@ if (require.main === module) {
 module.exports = findFirstDuplicate;
 
 // Please add your pseudocode to this file
+/*
+  -map the indexes of elements within the array that match, if no match is given the value for that elements index in the map will be -1
+  -filter out this map so that it contains only elements that are NOT -1
+  -if this filtered map has a length of 0 (no elements that are not of index -1) return -1, else return the value of the original
+   array at the index of the first value within the filtered map.
+*/
 // And a written explanation of your solution
+/*
+  In order to overcome the synchronus nature of javascript I had to create multiple functions that broke the solution down into steps.
+    each one called another one with the desired result being given at the end.  First I created an array of the indexes of all the
+    matches within an array.  If there were none I would return -1 but if their were multiple matches I had to call yet another
+    function that found the first index that repeated one after the other.  I would then use this index value as the return index
+    of the initial array.
+*/
