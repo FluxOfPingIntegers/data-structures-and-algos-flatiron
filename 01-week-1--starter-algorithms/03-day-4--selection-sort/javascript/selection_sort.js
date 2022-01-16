0f79bc22b8e8a7620408fdf880ddb32d0d48dbf2
FluxOfPingIntegers <Ryan.M.Schleck@gmail.com>
2022-01-16 11:47:21 -0500

    pseudo code added for javascript selection_sort

--- 01-week-1--starter-algorithms/03-day-4--selection-sort/javascript/selection_sort.js
+++ 01-week-1--starter-algorithms/03-day-4--selection-sort/javascript/selection_sort.js
@@ -1,5 +1,21 @@
 function selectionSort(arr) {
   // type your code here
+  let arrObj = {}
+  let i = -1
+  let answer = []
+
+  const populateArrObj = () => {
+    const populatedObjProgression = arr.map((element) => {
+      arrObj[`${element}`] = i++
+      return arrObj
+    })
+    return populatedObjProgression[populatedObjProgression.length - 1]
+  }
+
+  const findGreatestValue = (arrObj = populateArrObj()) => {
+    greaterKey = arrObj[`${i}`]
+
+  }
 }
 
 if (require.main === module) {
@@ -20,4 +36,16 @@ if (require.main === module) {
 module.exports = selectionSort;
 
 // Please add your pseudocode to this file
+/*
+  -create a blank array
+  -fill this array with elements representing indexes that is equal in length to the argument array
+  -create a blank object
+  -create a variable, set it to zero
+  -iterate through the argument array, assign each key using elements from the created array and the values using
+   the argument array
+  -create another blank array
+  -create a function that finds the lowest value in the hash if the value is true, pushes that number to the array, and 
+   sets that hash keys value to false.
+  -return this new array
+*/
 // And a written explanation of your solution
