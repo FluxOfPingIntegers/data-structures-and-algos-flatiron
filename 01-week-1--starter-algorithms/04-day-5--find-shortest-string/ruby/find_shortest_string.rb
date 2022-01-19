f4f5e06d8e09028ff8da83217aefd6ad35538056
FluxOfPingIntegers <Ryan.M.Schleck@gmail.com>
2022-01-19 11:56:35 -0500

    find_shortest_string ruby solve

--- 01-week-1--starter-algorithms/04-day-5--find-shortest-string/ruby/find_shortest_string.rb
+++ 01-week-1--starter-algorithms/04-day-5--find-shortest-string/ruby/find_shortest_string.rb
@@ -1,5 +1,12 @@
 def find_shortest_string(arr)
   # type your code in here
+  answer = (!!arr[0] ? arr[0] : "")
+  arr.each do |element|
+    if element.length < answer.length
+      answer = element
+    end
+  end
+  answer
 end
 
 if __FILE__ == $PROGRAM_NAME
@@ -22,4 +29,14 @@ if __FILE__ == $PROGRAM_NAME
 end
 
 # Please add your pseudocode to this file
+=begin
+  -create variable *answer equal to the first element in arr or an empty string
+  -iterate through arr, test element string length, if element.length < answer.length, answer = element
+  -return answer
+=end
 # And a written explanation of your solution
+=begin
+  I am creating a variable *answer set to the first element in arr or a blank string and testing its length against each
+  element in the argument array.  If an elements length is greater answer I am assigning the blank steam variable
+  to that element.  After this iteration is complete I am returning answer
+=end
