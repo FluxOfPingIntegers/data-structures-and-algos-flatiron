faef1fa171d9593c776df278c47dffdf24e31fe9
FluxOfPingIntegers <Ryan.M.Schleck@gmail.com>
2022-01-19 11:44:12 -0500

    find_shortest_string javascript solve

--- 01-week-1--starter-algorithms/04-day-5--find-shortest-string/javascript/find_shortest_string.js
+++ 01-week-1--starter-algorithms/04-day-5--find-shortest-string/javascript/find_shortest_string.js
@@ -1,5 +1,10 @@
 function findShortestString(arr) {
   // type your code here
+  let answer = (!!arr[0] ? arr[0] : "")
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].length < answer.length) {answer = arr[i]}
+    if (i === arr.length - 1) {return answer}
+  }
 }
 
 if (require.main === module) {
@@ -23,4 +28,14 @@ if (require.main === module) {
 module.exports = findShortestString;
 
 // Please add your pseudocode to this file
+/*
+  -create variable *answer equal to an empty string
+  -iterate through arr, test element string length, if element.length > answer, answer = element
+   if element === arr.last return answer
+*/
 // And a written explanation of your solution
+/*
+  I am creating a blank string and testing its length against each element in the argument array.  If an elements length
+  is greater than the blank string variable I am assigning the blank steam variable to that element.  On the last iteration I
+  am returning the variable that was originally blank.
+*/
