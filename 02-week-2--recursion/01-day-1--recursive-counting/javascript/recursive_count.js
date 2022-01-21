d012396c06ee64f875e3c5f5649903c7e37294c0
FluxOfPingIntegers <Ryan.M.Schleck@gmail.com>
2022-01-21 16:04:22 -0500

    recursive_count javascript solve

--- 02-week-2--recursion/01-day-1--recursive-counting/javascript/recursive_count.js
+++ 02-week-2--recursion/01-day-1--recursive-counting/javascript/recursive_count.js
@@ -1,5 +1,17 @@
 function recursiveCount(num = 0) {
   // type your code here
+  const addCount = (n) => {
+    if (n < 9) {
+      console.log(n)
+      addCount(n + 1)
+    } else {
+      console.log(n)
+      return n
+    }
+    return n
+  }
+
+  addCount(num)
 }
 
 if (require.main === module) {
