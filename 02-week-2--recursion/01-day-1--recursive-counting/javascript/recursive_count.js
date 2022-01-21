a34d6274baf2f6fbe670baf59a963bd2d3b63728
FluxOfPingIntegers <Ryan.M.Schleck@gmail.com>
2022-01-21 16:10:06 -0500

    pseudo code and explanation for recursive_count javascript solve

--- 02-week-2--recursion/01-day-1--recursive-counting/javascript/recursive_count.js
+++ 02-week-2--recursion/01-day-1--recursive-counting/javascript/recursive_count.js
@@ -22,4 +22,15 @@ module.exports = recursiveCount;
 
 // OPTIONAL
 // Please add your pseudocode to this file
+/*
+  -define a function with an if statement
+  -if argument is below top number of count, then print the argument followed by calling the function again with the arument + 1
+  -else print the argument followed by returning the argument
+  -outside of the if statement return the argument by default
+  -call the function with the initial argument
+*/
 // And a written explanation of your solution
+/*
+  I wanted to print the value of the argument each time as well as increment it and call the function again if the maximum value
+  had not been reached yet.  At the end of the function I wanted to return the value whether the condition was met or not.
+*/
