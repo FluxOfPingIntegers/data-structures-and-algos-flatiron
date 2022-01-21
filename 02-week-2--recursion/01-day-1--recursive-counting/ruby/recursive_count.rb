70974944a9d08024e6f5269cd02abc253e89e638
FluxOfPingIntegers <Ryan.M.Schleck@gmail.com>
2022-01-21 16:17:57 -0500

    recursive_count ruby solve

--- 02-week-2--recursion/01-day-1--recursive-counting/ruby/recursive_count.rb
+++ 02-week-2--recursion/01-day-1--recursive-counting/ruby/recursive_count.rb
@@ -1,5 +1,11 @@
 def recursive_count(num = 0)
   # type your code in here
+  puts num
+  if num < 9
+    recursive_count(num + 1)
+  else
+    nil
+  end
 end
 
 if __FILE__ == $PROGRAM_NAME
@@ -8,4 +14,13 @@ end
 
 # OPTIONAL
 # Please add your pseudocode to this file
+=begin
+  -print num
+  -if num is less than max count, call recursive_count(num + 1)
+  -else return nil
+=end
 # And a written explanation of your solution
+=begin
+  I printed num and then checked to see if the value was the maximum count yet
+  if it wasn't I incremented num and called the method again, if it was I simply returned nil
+=end
