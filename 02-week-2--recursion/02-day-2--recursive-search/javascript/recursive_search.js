fdaa732f0f03e6f7dba80a0dc34d84dea5cdfe4b
FluxOfPingIntegers <Ryan.M.Schleck@gmail.com>
2022-01-22 14:33:01 -0500

    recursive_search javascript solve

--- 02-week-2--recursion/02-day-2--recursive-search/javascript/recursive_search.js
+++ 02-week-2--recursion/02-day-2--recursive-search/javascript/recursive_search.js
@@ -1,5 +1,19 @@
 function recursiveSearch(arr, target) {
   // type your code here
+  let i = 0
+
+  const recursiveTest = (index) => {
+    while (index < arr.length) {
+      if (arr[index] == target) {
+        return true
+      } else {
+        return recursiveTest(i++)
+      }
+    }
+    return false
+  }
+
+  return recursiveTest(i)
 }
 
 if (require.main === module) {
@@ -16,4 +30,15 @@ if (require.main === module) {
 module.exports = recursiveSearch;
 
 // Please add your pseudocode to this file
+/*
+  -declare a variable *i equal to zero
+  -create a function that has a while loop that will run until i is less than the length of arr
+  -create an if statement within the while loop, if arr[i] === target return true, else increment i and call the function again
+  -outside of this while loop (and if else statement) inside of the function on the last line, return false
+*/
 // And a written explanation of your solution
+/*
+  I need to iterate through arr testing if any element matches the target using recursion.  The simplest way to do this is to
+  set a counter variable outside of the recursive function and increment it each time the function runs.  If the tested element
+  ever matches the target, return true.  If each element is tested within arr and none match the target, return false.  
+*/
