fe5c00c1324cbec05a384fe45d3f71e8e0bdde74
FluxOfPingIntegers <Ryan.M.Schleck@gmail.com>
2022-01-23 08:54:41 -0500

    ruby solve for fibonacci_recursive

--- 02-week-2--recursion/03-day-3--recursive-fibonacci-series/ruby/fibonacci_recursive.rb
+++ 02-week-2--recursion/03-day-3--recursive-fibonacci-series/ruby/fibonacci_recursive.rb
@@ -1,5 +1,9 @@
 def fibonacci(n)
   # type your code in here
+  if n == 0 || n == 1
+    return n
+  end
+  fibonacci(n - 1) + fibonacci(n - 2)
 end
 
 if __FILE__ == $PROGRAM_NAME
@@ -20,4 +24,15 @@ if __FILE__ == $PROGRAM_NAME
 end
 
 # Please add your pseudocode to this file
+=begin
+  -if n < 2 return n
+  -else decrease n by 1 and add it to n decreased by 2 until condition is met, calling the function recursively
+=end
 # And a written explanation of your solution
+=begin
+  Because I was only able to hold onto one number throughout this I needed to turn the problem into a series of 1 + 1 + 1...etc.
+  The only way to store all of these ones was to make a condition at the start of the method that would return n if it were ever one
+  However I also needed to account for if n was originally 0 so 0 needed to be returned if this was the case, hence the or statement.
+  If I added two calls of this method together while decreasing n each time they were called I would eventually get the series of
+  addition (1 + 1 + 1...etc) that I had hoped for.  Once all of these method calls had values the return would add them all together.
+=end
