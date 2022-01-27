1c1e4f7cfecb0b8acb364bbde57e24eee14cc841
FluxOfPingIntegers <Ryan.M.Schleck@gmail.com>
2022-01-27 11:56:32 -0500

    balancing_parentheses ruby solve

--- 03-week-3--additional-practice/00-bonus-1--balancing-parenetheses/ruby/balancing_parentheses.rb
+++ 03-week-3--additional-practice/00-bonus-1--balancing-parenetheses/ruby/balancing_parentheses.rb
@@ -1,5 +1,24 @@
 def balancing_parentheses(string)
   # type your code in here
+  string_chars = string.split('')
+  f = 0
+  b = 0
+  string_chars.each do |char|
+    if char == "("
+      f += 1
+    next
+    else
+      b += 1
+    next
+    end
+  end
+  f >= b ? greater = f : greater = b
+  f <= b ? lesser = f : lesser = b
+  if string_chars[0] == ")" && string_chars[0] != string_chars.last
+    (greater - lesser) + 2
+  else
+    greater - lesser
+  end
 end
 
 if __FILE__ == $PROGRAM_NAME
