73aeea1a4c1eb4b10118fb64d4b7362a88eaa576
FluxOfPingIntegers <Ryan.M.Schleck@gmail.com>
2022-01-31 12:16:27 -0500

    rotate_array javascript solve

--- 03-week-3--additional-practice/02-bonus-3--rotate-array-clockwise/javascript/rotate_array.js
+++ 03-week-3--additional-practice/02-bonus-3--rotate-array-clockwise/javascript/rotate_array.js
@@ -1,5 +1,14 @@
 function rotateArray(arr, k) {
   // type your code here
+  if (k <= 0 || arr.length <= 1) {
+    return arr
+  } else {
+    const rotateElement = arr.pop()
+    const newArr = [rotateElement, ...arr]
+    const newK = k - 1
+
+    return rotateArray(newArr, newK)
+  }
 }
 
 if (require.main === module) {
@@ -21,4 +30,19 @@ if (require.main === module) {
 module.exports = rotateArray;
 
 // Please add your pseudocode to this file
+/*
+  -if the length of the array is less than or equal to 1, or if the count is less than or equal to 0 return the array
+  -else create variable *rotateElement equal to the last element of the array and mutate array to exclude rotateElement
+  -create a new variable *newArr which is equal to an array consisting of rotateElement as the first element followed
+   by the remainder of the argument array
+  -create a new variable *newK which is equal to the k argument minus 1
+  -return a recursive call of this function with the arguments newArr and newK
+*/
 // And a written explanation of your solution
+/*
+  If the array was empty, or only had one element I wanted to return the array.  If that wasn't the case I needed to
+  get the last element of the argument array saved to a variable and remove it from the array, then I needed to add that
+  element to the front of the array.  I then needed to decrement the count argument by 1 and return the outcome of this
+  function being called again with this new array and new count number.  The result being that the count will eventually
+  reach zero and the first if statement will return the argument array.
+*/
