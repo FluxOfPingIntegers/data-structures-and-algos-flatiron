c0a9f64c853fbd6ebbf045320b6c02b026122a2a
FluxOfPingIntegers <Ryan.M.Schleck@gmail.com>
2022-01-31 12:31:08 -0500

    rotate_array ruby solve

--- 03-week-3--additional-practice/02-bonus-3--rotate-array-clockwise/ruby/rotate_array.rb
+++ 03-week-3--additional-practice/02-bonus-3--rotate-array-clockwise/ruby/rotate_array.rb
@@ -1,5 +1,12 @@
 def rotate_array(arr, k)
   # type your code in here
+  if arr.length <= 1 || k <= 0
+    arr
+  else
+    rotate_element = arr.pop
+    new_arr = arr.unshift(rotate_element)
+    rotate_array(new_arr, k - 1)
+  end
 end
 
 if __FILE__ == $PROGRAM_NAME
@@ -22,4 +29,18 @@ if __FILE__ == $PROGRAM_NAME
 end
 
 # Please add your pseudocode to this file
+=begin
+  -if the length of the array is less than or equal to 1, or if the count is less than or equal to 0 return the array
+  -else create variable *rotate_element equal to the last element of the array and mutate the array to exclude rotate_element
+  -create a new variable *new_arr which is equal to an array consisting of rotate_element as the first element followed
+   by the remainder of the argument array
+  -return a recursive call of this method with the arguments new_arr and k - 1
+=end
 # And a written explanation of your solution
+=begin
+  If the array was empty, or only had one element I wanted to return the array.  If that wasn't the case I needed to
+  get the last element of the argument array saved to a variable and remove it from the array, then I needed to add that
+  element to the front of the array.  I then needed return the outcome of this method being called again with this new array
+  and the count number being decremented by 1.  The result being that the count will eventually reach zero and the first if 
+  statement will return the argument array.
+=end
