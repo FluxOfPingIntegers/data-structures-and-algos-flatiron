c2ef696474279df6bd08948db74b6086ee4b7caa
FluxOfPingIntegers <Ryan.M.Schleck@gmail.com>
2022-02-06 08:39:05 -0500

    distinct_pair_sum javascript solve

--- 03-week-3--additional-practice/03-bonus-4--distinct-pair-sum/javascript/distinct_pair_sum.js
+++ 03-week-3--additional-practice/03-bonus-4--distinct-pair-sum/javascript/distinct_pair_sum.js
@@ -1,27 +1,42 @@
 function distinctPairSum(arr, k) {
   // type your code here
-  let obj= {}
-  const populateArrObj = () => {arr.forEach((element, index) => {
-    obj[`${element}`] = element
-    if (index === arr.length - 1) {
-      console.log(obj)
-      return obj
+  if (arr.length <= 1) {
+    return []
+  }
+
+  let arrObj = {}
+  const populateArrObj = (count = 0) => {
+    if (count === arr.length) {
+      return arrObj
+    } else {
+      arrObj[`${arr[count]}`] = arr[count] !== 0 ? arr[count] : true
+      return populateArrObj(count += 1)
     }
-    })
   }
 
-  const distinctPair = (arrObj = populateArrObj()) => {
-    return arr.map((element) => {
-      const value = k - element
-      if (!!arrObj[`${element}`] && !!arrObj[`${value}`]) {
-        arrObj[`${element}`] = false
-        arrObj[`${value}`] = false
-        return [element, value]
-      }
-    })
+  const distinctPairSumArr = ({arrObj, sumArr, count}) => {
+
+    const hasDuplicate = element => arr.indexOf(element) !== arr.lastIndexOf(element)
+
+    const element = !!arr[count] ? arr[count] : false
+    const value = !!element ? k - element : false
+    const duplicateTest = element !== value ? true : hasDuplicate(element)
+
+    const newSumArr = !!arrObj[`${element}`] &&
+     !!arrObj[`${value}`] &&
+     duplicateTest ? sumArr.concat([[element, value]]) : sumArr
+
+    const newArrObj = !!arrObj[`${element}`] &&
+     !!arrObj[`${value}`] ? Object.assign(arrObj, {[element]: false}, {[value]: false}) : arrObj
+
+    if (count === arr.length) {
+      return (k === 0 && hasDuplicate(k) ? sumArr.concat([[0, 0]]) : sumArr)
+    } else {
+      return distinctPairSumArr({arrObj: newArrObj, sumArr: newSumArr, count: count + 1})
+    }
   }
 
-  return distinctPair()
+  return distinctPairSumArr({arrObj: populateArrObj(), sumArr: [], count: 0})
 }
 
 if (require.main === module) {
@@ -39,9 +54,16 @@ module.exports = distinctPairSum;
 
 // Please add your pseudocode to this file
 /*
-  -map all of arrs values to hash so that each of them are unique
-  -iterate through arr, subtract each value from k and test to see if hash has the key, if it does set both the element key and
-   value key to false.
-  -once iteration is through return mapped array
+  -if arr has 1 or less elements in it.  return empty array.
+  -create new object called *arrObj, iterate over arr adding each element as the key/value to arrObj, if element is 0 value is true.
+  -iterate over over arr once again using arrObj, a counter called *count, and a sum array you will return called *sumArr
+   within each iteration find out if k - arr[count] is present in arrObj, if it is add [arr[count], k - arr[count]] to sumArr
+   also test each element for duplicates within arr incase arr[count] is half of k *the total you're seeking
+  -once count is equal to arr.length, return sumArr
 */
 // And a written explanation of your solution
+/*
+  I wanted to create an object representing elements in arr to reduce lookup times.  I knew I had to build the answer through
+  recursion in order for each iteration to finish before the next one began.  Special test/considerations had to be taken in
+  the case that k was 0 or if there were duplicates within arr.
+*/
