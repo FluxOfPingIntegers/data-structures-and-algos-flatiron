cfa29a021e9ce8854d69dc12cf5116d5428b147e
FluxOfPingIntegers <Ryan.M.Schleck@gmail.com>
2022-02-06 09:23:43 -0500

    distinct_pair_sum ruby solve

--- 03-week-3--additional-practice/03-bonus-4--distinct-pair-sum/ruby/distinct_pair_sum.rb
+++ 03-week-3--additional-practice/03-bonus-4--distinct-pair-sum/ruby/distinct_pair_sum.rb
@@ -1,5 +1,19 @@
 def distinct_pair_sum(arr, k)
   # type your code in here
+  arr_hash = {}
+  index_arr = (0...(arr.length - 1))
+
+  index_arr.each do |i|
+    next_element = arr[i + 1]
+
+    if arr[i] + next_element == k &&
+      !arr_hash.key?(next_element) && 
+      !arr_hash.key?(arr[i])
+      arr_hash[arr[i]] = [arr[i], next_element]
+    end
+  end
+
+  arr_hash.values
 end
 
 if __FILE__ == $PROGRAM_NAME
@@ -18,9 +32,17 @@ end
 
 # Please add your pseudocode to this file
 =begin
-
+  -create hash to hold elements as key/values called *arr_hash
+  -create array to hold indexes of arr called *index_arr
+  -iterate over index_arr using *i as the current index
+  -if arr[i] + arr[i + 1] = k and arr_hash[arr[i]] is false and arr_hash[arr[i +1]] is false, arr_hash[arr[i]] = [arr[i], arr[i + 1]]
+  -return all the values of arr_hash (there will be no duplicates since they're constantly overwritten)
 =end
 # And a written explanation of your solution
 =begin
-
+  I needed to create a hash in order to store all all of the elements within arr that add up to k.  I also needed to create an
+  array that contained all of the indexes in arr as elements.  I then iterated over the index array and tested if the current
+  index element in arr added to the next index element in arr would equal k.  If they did I used the current index element in
+  arr as the key in the hash and gave it the value of [current, next].  Once this iteration was completed I returned the values
+  within the hash.
 =end
