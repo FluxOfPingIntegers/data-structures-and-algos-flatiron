ceb1eb56c508758a6f6ac363e562f88cc63dbf7f
FluxOfPingIntegers <Ryan.M.Schleck@gmail.com>
2022-02-07 16:53:34 -0500

    consecutive_substrings javascript solve

--- 03-week-3--additional-practice/04-bonus-5--consecutive-substrings/javascript/consecutive_substrings.js
+++ 03-week-3--additional-practice/04-bonus-5--consecutive-substrings/javascript/consecutive_substrings.js
@@ -1,5 +1,23 @@
 function consecutiveSubstrings(string) {
   // type your code here
+  if (string.length <= 1) {return string.split('')}
+  const characters = string.split('')
+  const numberOfSubstrings = ({num, count}) => {return num > 0 ? numberOfSubstrings({num: num - 1, count: count + num}) : count}
+  const subCount = numberOfSubstrings({num: characters.length, count: 0})
+
+  const populateSubstringArr = ({subArr, count, nextCount, res}) => {
+    if (subArr.length < subCount) {
+      const currentElement = res ? characters[count] : subArr[subArr.length - 1] + characters[count]
+      if (count === characters.length - 1) {
+        return populateSubstringArr({subArr: subArr.concat([currentElement]), count: nextCount, nextCount: nextCount + 1, res: true})
+      } else {
+        return populateSubstringArr({subArr: subArr.concat([currentElement]), count: count + 1, nextCount: nextCount, res: false})
+      }
+    } else {
+      return subArr
+    }
+  }
+  return populateSubstringArr({subArr: [], count: 0, nextCount: 1, res: true})
 }
 
 if (require.main === module) {
