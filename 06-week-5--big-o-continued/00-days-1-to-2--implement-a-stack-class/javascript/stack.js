7758d7eb393ac08f40c372ec98427a217259a333
FluxOfPingIntegers <Ryan.M.Schleck@gmail.com>
2022-02-09 15:51:13 -0500

    implement-a-stack-class javascript solve

--- 06-week-5--big-o-continued/00-days-1-to-2--implement-a-stack-class/javascript/stack.js
+++ 06-week-5--big-o-continued/00-days-1-to-2--implement-a-stack-class/javascript/stack.js
@@ -9,43 +9,47 @@ class Stack {
   // add item to top of stack if not full
   // if full throw error
   push(item) {
-
+    if (!this.isFull()) {
+      return this.stack.push(item)
+    } else {
+      throw new Error('The Stack Is Full!')
+    }
   }
 
   // remove item from top of stack and return it
   pop() {
-
+    return this.stack.pop()
   }
 
   // return item at top of stack without removing it
   peek() {
-
+    return !!this.stack.length > 0 ? this.stack[this.stack.length - 1] : undefined
   }
 
   // return true if stack is empty, otherwise false
   isEmpty() {
-
+    return this.size() === 0
   }
 
   // return true if stack is full, otherwise false
   isFull() {
-
+    return this.stack.length >= this.limit
   }
 
   // return number of items in stack
   size() {
-
+    return this.stack.length
   }
 
   // return -1 if item not in stack, otherwise integer representing 
   // how far it is from the top
   search(target) {
-
+    return !!this.stack.find(element => element === target) ? (this.stack.length - 1) - this.stack.findIndex(element => element === target) : -1
   }
 
   // print contents of stack: do not return the stack itself!
   print() {
-    
+    console.log(this.stack)
   }
 }
 
