5cf92572b174636361729207717360b64437e2cd
FluxOfPingIntegers <Ryan.M.Schleck@gmail.com>
2022-02-24 21:33:17 -0500

    implement-a-stack-class ruby solve

--- 06-week-5--big-o-continued/00-days-1-to-2--implement-a-stack-class/ruby/stack.rb
+++ 06-week-5--big-o-continued/00-days-1-to-2--implement-a-stack-class/ruby/stack.rb
@@ -11,35 +11,51 @@ class Stack
   # add item to top of stack if not full
   # if full, throw error
   def push(item)
+    if !isFull?
+      @stack.push(item)
+    else
+      raise 'Stack is full'
+    end
   end
 
   # remove item from top of stack and return it
   def pop
+    @stack.slice!(size - 1)
   end
 
   # return item at top of stack without removing it
   def peek
+    self.stack.last
   end
 
   # return true if stack is empty, otherwise false
   def isEmpty?
+    self.size === 0
   end
 
   # return true if stack is full, otherwise false
   def isFull?
+    size === @limit
   end
 
   # return number of items in stack
   def size
+    @stack.length
   end
 
   # return -1 if item not in stack, otherwise integer representing 
   # how far it is from the top
   def search(target)
+    if @stack.include?(target)
+      (size - 1) - @stack.index(target)
+    else
+      -1
+    end
   end
 
   # print contents of stack: do not return the stack itself!
   def print
+    puts @stack
   end
 end
 
